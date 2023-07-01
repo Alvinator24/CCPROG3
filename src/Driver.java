b721a62d9d0a7d40304bc24eccdce6325d263065
Alvinator24 <alvinalvin3065@gmail.com>
2023-07-01 20:43:01 +0800

    user input added

--- src/Driver.java
+++ src/Driver.java
@@ -3,8 +3,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Driver {
-
-
     
     // we only need to store the data (vending machines), so ArrayList is optimal
     // if we needed to manipulate the data, LinkedLists are better (internally doubly linked lists)
@@ -25,43 +23,52 @@ public class Driver {
         denoms.add(new Denomination(500));
         denoms.add(new Denomination(1000));
 
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
 
 
+        while(choice != 3) {
+            System.out.println("1. Create RVM");
+            System.out.println("2. Create SVM");
+            System.out.println("3. Exit");
+
+            if(choice == 1) {
+                VendingMachine test = new VendingMachine("test", 10,10, denoms );
+
+                Item iceTea = new Item("IceTea", 65, 300);
+                Item orangeJuice = new Item("Orangey", 165, 3300);
+                Item appleJuice = new Item("Appley", 101, 250);
+                Item iceTea2 = new Item("IceTea", 65, 300);
+
+                test.restockItem(new Item("IceTea", 65, 300));
+                test.restockItem(new Item("IceTea", 65, 300));
+                test.restockItem(new Item("IceTea", 65, 300));
+                test.restockItem(new Item("IceTea", 65, 300));
+                test.restockItem(new Item("IceTea", 65, 300));
+                test.restockItem(new Item("IceTea", 65, 300));
+
+                test.restockItem(new Item("Orangey", 165, 3300));
+                test.restockItem(new Item("Appley", 101, 250));
 
 
-        VendingMachine test = new VendingMachine("test", 10,10, denoms );
+                test.purchaseItem(1);
+                test.purchaseItem(1);
+                test.purchaseItem(2);
 
-        Item iceTea = new Item("IceTea", 65, 300);
-        Item orangeJuice = new Item("Orangey", 165, 3300);
-        Item appleJuice = new Item("Appley", 101, 250);
-        Item iceTea2 = new Item("IceTea", 65, 300);
+                test.currentTransaction.removeItem(3);
 
-        test.addItem(iceTea);
-        test.addItem(orangeJuice);
-        test.addItem(appleJuice);
+                test.dispenseCoin(20);
+                test.dispenseCoin(1);
+                test.dispenseCoin(.25);
+            }
 
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
+            sc.close();
 
-        test.restockItem(new Item("Orangey", 165, 3300));
-        test.restockItem(new Item("Appley", 101, 250));
+        }
 
-
-        test.purchaseItem(1);
-        test.purchaseItem(1);
-        test.purchaseItem(2);
-
-        test.currentTransaction.removeItem(3);
-
-        test.dispenseCoin(20);
-        test.dispenseCoin(1);
-        test.dispenseCoin(.25);
-        test.currentTransaction.displayTransaction();
-
+        
+        //test.currentTransaction.displayTransaction();
+        
 
         //test.displayItems();
         //System.out.println(test.currentTransaction.cartedItems.get(0).getItemName());
