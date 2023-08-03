0b005126240c8ab3464c53afd30bca746a8a0581
Jabberino <122722580+Jabberino@users.noreply.github.com>
2023-08-04 02:00:16 +0800

    DONE

--- src/Driver.java
+++ src/Driver.java
@@ -1,19 +1,14 @@
-// import java.util.ArrayList;
-import java.util.ArrayList;
-import java.util.Scanner;
 
+import java.util.ArrayList;
+
+/**
+ * Starts the whole program
+ */
 public class Driver {
 
-
-    
-    // we only need to store the data (vending machines), so ArrayList is optimal
-    // if we needed to manipulate the data, LinkedLists are better (internally doubly linked lists)
-
     public static void main(String[] args) {
+
         ArrayList<Denomination> denoms = new ArrayList<Denomination>();
-        denoms.add(new Denomination(.01));
-        denoms.add(new Denomination(.05));
-        denoms.add(new Denomination(.1));
         denoms.add(new Denomination(.25));
         denoms.add(new Denomination(1));
         denoms.add(new Denomination(5));
@@ -25,56 +20,6 @@ public class Driver {
         denoms.add(new Denomination(500));
         denoms.add(new Denomination(1000));
 
-
-
-
-
-        VendingMachine test = new VendingMachine("test", 10,10, denoms );
-
-        Item iceTea = new Item("IceTea", 65, 300);
-        Item orangeJuice = new Item("Orangey", 165, 3300);
-        Item appleJuice = new Item("Appley", 101, 250);
-        Item iceTea2 = new Item("IceTea", 65, 300);
-
-        test.addItem(iceTea);
-        test.addItem(orangeJuice);
-        test.addItem(appleJuice);
-
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-        test.restockItem(new Item("IceTea", 65, 300));
-
-        test.restockItem(new Item("Orangey", 165, 3300));
-        test.restockItem(new Item("Appley", 101, 250));
-
-
-        test.purchaseItem(1);
-        test.purchaseItem(1);
-        test.purchaseItem(2);
-        test.purchaseItem(3);
-
-        //test.currentTransaction.removeItem(3);
-
-        test.dispenseCoin(20);
-        test.dispenseCoin(1);
-        test.dispenseCoin(1000);
-
-        test.displayTransaction();
-
-        test.checkout(true);
-
-        //test.displayItems();
-        //System.out.println(test.currentTransaction.cartedItems.get(0).getItemName());
-        //System.out.println(test.getSlot(iceTea2).getItem().getItemName());
+        Factory vendingFactory = new Factory(denoms);
     }
-
-    // MCO2 (Pizza Ingredients)
-    // Thin or thick crust
-    // Sauce
-    // Toppings
-    // cannot buy alone: dough, sauce
-
 }
